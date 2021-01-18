32cb6d64609cfc58e7727aabfb119e62278941eb
Alex Giorgio Asole <alexgiorgio.asole@gmail.com>
2021-01-18 15:46:45 +0100

    parte 2

--- js/script.js
+++ js/script.js
@@ -5,6 +5,27 @@ var alex = {
   'cognome': 'Asole',
   'età': 24
 }
+var gian = {
+  'nome': 'Gianpaolo',
+  'cognome': 'Garbarino',
+  'età': 42
+}
+var ale = {
+  'nome': 'Alessandro',
+  'cognome': 'Liggeri',
+  'età': 25
+}
 for (var key in alex) {
   console.log(alex[key])
+}
+
+/*Creare un array di oggetti di studenti.
+ Ciclare su tutti gli studenti e stampare per ognuno nome e cognome*/
+var studenti = [alex, gian, ale]
+for (var i = 0; i < studenti.length; i++) {
+  for (var key in studenti[i]) {
+    if(key !== 'età'){
+      console.log(studenti[i][key])
+    }
+  }
 }
