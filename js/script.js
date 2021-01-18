457273d985ea3daaa70ddd533de625353fce8912
Alex Giorgio Asole <alexgiorgio.asole@gmail.com>
2021-01-18 16:01:07 +0100

    parte 3

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-/* Creare un oggetto che descriva uno studente con le seguenti proprietà:
- nome, cognome e età. Stampare a schermo attraverso il for tutte le proprietà.*/
 var alex = {
   'nome': 'Alex Giorgio',
   'cognome': 'Asole',
@@ -15,17 +13,30 @@ var ale = {
   'cognome': 'Liggeri',
   'età': 25
 }
+var altroStudente = {}
+var studenti = [alex, gian, ale];
+/* Creare un oggetto che descriva uno studente con le seguenti proprietà:
+ nome, cognome e età. Stampare a schermo attraverso il for tutte le proprietà.*/
 for (var key in alex) {
-  console.log(alex[key])
+  console.log(alex[key]);
 }
-
+/*Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente
+ inserendo nell’ordine: nome, cognome e età. */
+ var nomeStud = prompt('Inserisci il tuo nome!');
+ var cognomeStud = prompt('Inserisci il tuo cognome!');
+ var etaStud = parseInt(prompt('Inserisci la tua età!'));
+/*Assegno ogni elemento inserito a un attributo del oggetto*/
+ altroStudente.nome = nomeStud;
+ altroStudente.cognome = cognomeStud;
+ altroStudente.età = etaStud;
+/*inserisco nell'array l'oggetto generato*/
+ studenti.push(altroStudente);
 /*Creare un array di oggetti di studenti.
  Ciclare su tutti gli studenti e stampare per ognuno nome e cognome*/
-var studenti = [alex, gian, ale]
-for (var i = 0; i < studenti.length; i++) {
-  for (var key in studenti[i]) {
-    if(key !== 'età'){
-      console.log(studenti[i][key])
-    }
-  }
-}
+ for (var i = 0; i < studenti.length; i++) {
+   for (var key in studenti[i]) {
+     if(key !== 'età'){
+       console.log(studenti[i][key]);
+     }
+   }
+ }
